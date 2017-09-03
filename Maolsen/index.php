f1ba301b4f10fbb907576f5176022e8422433c56
Marcus Olsen <maolsen@csumb.edu>
2017-09-03 04:09:09 +0000

    hello again

--- Maolsen/index.php
+++ Maolsen/index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <meta charset="utf-8"/>
+        <title>index.php</title>
     </head>
     <body>
         <a href="/lab1/index.html">Trykk her</a>
