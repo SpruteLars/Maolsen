3f82d6c619170103c298b0981dc63d65c2365a7b
Marcus Olsen <maolsen@csumb.edu>
2017-10-10 02:53:06 +0000

    hey

--- Maolsen/Homework/Hw3/index.php
+++ Maolsen/Homework/Hw3/index.php
@@ -32,24 +32,31 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <link rel="stylesheet" href="style.css" type="text/css" />
     </head>
     <body>
         <form>
-            <input type="text" name="x"/>
-            <input type="text" name="y"/>
+            <input type="number" name="x" placeholder="x = input. Default is 0"/>
+            <input type="number" name="y" placeholder="y = input. Default is 0"/>
             <input type="radio" name="PlussMinus" value="-"/>Minus
             <input type="radio" name="PlussMinus" value="+"/>Pluss
             <select name="DivideMultiply">
-                <option value=""></option>
+                <option value="" >Divide or Multiply</option>
                 <option value="/">Divide</option>
                 <option value="*">Multiply</option>
                 
             </select>
-            <input type="text" name="divisionMultiply"/>
+            <input type="text" name="divisionMultiply" placeholder="Number to divide or multiply with"/>
             <input type="submit" value="Submit"/>
         </form>
         <?php
+         if($x==""){
+                $x = 0;
+            }
+            if($y==""){
+                $y = 0;
+            }
+            
             echo "(";
             echo $x . " ";
             echo $plussminus . " ";
@@ -58,7 +65,7 @@
             echo $divideMultiply . " ";
             echo $divisionOrMultiplication . " ";
             echo "= ";
-            //if()
+           
             if ($plussminus == "+") {
                 
                 $sum = $x + $y;
@@ -67,15 +74,42 @@
                 
                 $sum = $x - $y;
             }
+            if($divideMultiply == ""){
+                echo  "<span>". $sum."</span>";
+            }
             if($divideMultiply == "*"){
-            
-                 $sum = $sum * $divisionOrMultiplication;
-            }
-            if($divideMultiply == "/"){
+                if (!is_numeric($divisionOrMultiplication)) {
+                  echo   $sum;
+                  echo   $divisionOrMultiplication;
+                } else {
+                   $sum = $sum * $divisionOrMultiplication;
+                 echo  "<span>". $sum."</span>";
+                 
+                }
                 
-                 $sum = $sum / $divisionOrMultiplication;
+                 
             }
-            echo  $sum;
+            
+            if($divideMultiply == "/"){
+                if(!is_numeric($divisionOrMultiplication)){
+                        echo $sum;
+                        echo $divideMultiply . " ";
+                        echo $divisionOrMultiplication . " ";
+                    }else{
+                        if ($divisionOrMultiplication == 0) {
+                            echo "<h2> Error: Can not divide by 0</h2>";
+                        }else{
+                
+                            $sum = $sum / $divisionOrMultiplication;
+                            echo  "<span>". $sum."</span>";
+                        }
+                    }
+               
+                    
+                }
+                
+            
+            
            
         ?>
     </body>
