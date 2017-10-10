3d55d60bc0e4dfee15a4c1e96dd73a87ac500c72
Marcus Olsen <maolsen@csumb.edu>
2017-10-10 05:58:14 +0000

    hey

--- Maolsen/Homework/Hw3/index.php
+++ Maolsen/Homework/Hw3/index.php
@@ -38,38 +38,40 @@
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
-        <form>
-            <input type="number" name="x" placeholder="x = input. Default is 0"/>
-            <input type="radio" name="PlussMinus" value="-"/>Minus
-            <input type="radio" name="PlussMinus" value="+"/>Pluss
-            <input type="number" name="y" placeholder="y = input. Default is 0"/><br>
-            <br>
-            <select name="DivideMultiply">
-                <option value="" >Divide or Multiply</option>
-                <option value="/">Divide</option>
-                <option value="*">Multiply</option>
-                
-            </select>
-            <input type="text" name="divisionMultiply" placeholder=" variable or number"/> <br>
-            <input type="submit" value="Calculate"/>
-        </form>
+        <div class="container">
+          <div class="jumbotron">
+                <form>
+                    <input type="number" name="x" placeholder="x = input. Default is 0"/>
+                    <input type="radio" name="PlussMinus" value="-"/>Minus
+                    <input type="radio" name="PlussMinus" value="+"/>Pluss
+                    <input type="number" name="y" placeholder="y = input. Default is 0"/><br>
+                    <br>
+                    <select name="DivideMultiply">
+                        <option value="" >Divide or Multiply</option>
+                        <option value="/">Divide</option>
+                        <option value="*">Multiply</option>
+                    
+                    </select>
+                    <input type="text" name="divisionMultiply" placeholder=" variable or number"/> <br>
+                    <input type="submit" value="Calculate"/>
+                </form>
         <?php
         
       
-        if ($x=="" && $y=="" ) {
-         echo "<h2> insert a value too either x or y</h2>";
+        if ($x=="" || $y=="" ) {
+            if($x==""){
+              echo "<span><h2>Error: insert a value for x</h2></span>";
+            }
+            if($y==""){
+              echo "<h2>Error: insert a value for y</h2>";
+            }
+            
         }else{
             if (!isset($_GET['PlussMinus'])) {
                 echo "<h2> Error: choose a value if you just want one number do x or y, + or - 0</h2>";
             } else {
                    
-         if($x==""){
-                $x = 0;
-            }
-            if($y==""){
-                $y = 0;
-            }
-            
+         
             echo "(";
             echo $x . " ";
             echo $plussminus . " ";
@@ -128,8 +130,8 @@
             
            
         ?>
-     
-        
+        </div>
+        </div>
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
             
