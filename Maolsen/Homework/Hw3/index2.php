a4597a189d7e74352f798e7d024f32f025c0c224
Marcus Olsen <maolsen@csumb.edu>
2017-10-10 05:44:31 +0000

    hey

--- Maolsen/Homework/Hw3/index2.php
+++ Maolsen/Homework/Hw3/index2.php
@@ -128,7 +128,56 @@
             
            
         ?>
-     
+        <br>
+        <br>
+        <br>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                 <?php
+                    for ($i = 0;$i < 7; $i++){
+                        echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
+                        echo ($i == 0)?"class ='active'": "";
+                        echo "></li>";
+                    }
+                    ?>
+              </ol>
+            
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  <img src="<?=$imageURLs[0]?>" alt="...">
+                  <div class="carousel-caption">
+                    
+                  </div>
+                </div>
+                <?php
+                for ($i = 0; $i < 7; $i++) {
+                    do {
+                       $randomIndex = rand(0,count($imageURLs));
+                    } while (!isset($imageURLs[$randomIndex]));
+                    echo '<div class="item ';
+                    echo ($i==0)?"active": "";
+                    echo '"/>';
+                    echo '<img src="' . $imageURLs[rand(0,$randomIndex)]. '" />';
+                    echo '</div>';
+                    unset($imageURLs[$randomIndex]);
+                }
+            
+                ?>           
+                <p> Math related picture from pixabay</p>
+              </div>
+            
+              <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
         
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
