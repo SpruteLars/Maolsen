97da486d336b375cb53acb398927c52ad78ea57f
Marcus Olsen <maolsen@csumb.edu>
2018-01-24 16:40:49 +0000

    setup

---
+++ Maolsen/CST336/labs/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <nav>
+            <a herf="index.php">Home</a>
+            <a herf="about.php">About</a>
+            <a herf="contact.php">Contact</a>
+            
+        </nav>
+    </body>
+</html>
