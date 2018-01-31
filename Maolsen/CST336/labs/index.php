31c3ac856dbbe1d3b1a82be2fd0117c31bbe523e
Marcus Olsen <maolsen@csumb.edu>
2018-01-31 17:27:48 +0000

    Lab1

--- Maolsen/CST336/labs/index.php
+++ Maolsen/CST336/labs/index.php
@@ -1,16 +1,39 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Marcus's website</title>
+        <meta charset="utf-8"/>
         <link rel="stylesheet" href="style.css" type="text/css" />
     </head>
-    <body>
-        <nav>
-            <a href="index.php" style='text-decoration:underline'>Home</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact</a>
+       <body style="background-color:green;">
+        <header>
+            <h1>Marcus Olsen</h1>
+        </header>
+        <hr>
+             <nav>
+                <a href="index.php" style='text-decoration:underline'>Home</a>
+                <a href="about.html">About</a>
+                <a href="contact.html">Contact</a>
+            </nav>
             
-        </nav>
+           
+
+       
         
         <img src="cst352.PNG"></img>
+        <aside>
+         <div>
+                 <p>
+                 Thank you for visiting my Website!
+                 </p>   
+            </div>
+            </aside>
+        <hr>
+        <footer>
+            
+             <img src="logo1.png" id="logo"></img>
+             
+             
+        </footer>
     </body>
 </html>
