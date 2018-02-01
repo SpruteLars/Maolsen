6f860e711ea21de736cfa20e3f8c67a77f19a9c0
Marcus Olsen <maolsen@csumb.edu>
2018-02-01 05:37:32 +0000

    final commit lab1

--- Maolsen/CST336/labs/index.php
+++ Maolsen/CST336/labs/index.php
@@ -9,12 +9,13 @@
         <header>
             <h1>Marcus Olsen</h1>
         </header>
-        <hr>
-             <nav>
+          <nav>
                 <a href="index.php" style='text-decoration:underline'>Home</a>
                 <a href="about.html">About</a>
                 <a href="contact.html">Contact</a>
             </nav>
+        <hr>
+           
             
            
 
@@ -24,7 +25,12 @@
         <aside>
          <div>
                  <p>
-                 Thank you for visiting my Website!
+                 Thank you for visiting my Website!<br>
+                 <br>
+                 
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pharetra facilisis lorem nec tincidunt. Suspendisse aliquet tincidunt ultricies. Vivamus leo lectus, tristique a luctus sit amet, sollicitudin ac augue. Sed purus ex, ultrices non nulla vel, fermentum ullamcorper urna. Nunc vehicula fermentum eros, sed tempus est elementum ut. Sed pulvinar dolor justo, non auctor sem suscipit nec. Phasellus sed pharetra dui. Vestibulum sed ultricies tortor. Nulla ut urna eu leo volutpat faucibus. Sed at quam egestas, egestas erat ut, iaculis massa. Praesent in lorem nisi. Pellentesque sed orci rhoncus, mattis odio quis, consectetur neque. Donec auctor ipsum nec est finibus, eu faucibus nulla dapibus. Duis quis erat auctor, molestie arcu ut, aliquet ligula.
+
+Ut suscipit vel lectus sit amet ultrices. Sed bibendum justo sit amet turpis efficitur, fringilla rhoncus velit fringilla. Duis mauris risus, euismod sed fringilla id, tempor sed dolor. Sed convallis tellus quis urna porttitor suscipit. Suspendisse vitae felis iaculis, varius nulla et, dapibus sem. Aenean lobortis consequat nunc nec accumsan. Mauris leo urna, faucibus ornare blandit vel, hendrerit vel odio. Pellentesque purus risus, consectetur sit amet pharetra eu, congue vestibulum dui. Cras consectetur, neque sed facilisis maximus, nibh nibh iaculis risus, sed ornare nulla lorem malesuada metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt quam a augue tincidunt, eget sollicitudin tortor ultricies. Ut fermentum lacinia elit. In id eros eu nisl tempus ultrices vitae id lorem. Maecenas egestas commodo risus, eget pretium libero pretium ut. Integer tincidunt rutrum felis, quis faucibus dui mollis ut.
                  </p>   
             </div>
             </aside>
